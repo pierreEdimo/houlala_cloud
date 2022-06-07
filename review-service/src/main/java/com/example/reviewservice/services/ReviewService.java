d78252078ae13eccb882653646ad30b45c733892
pierre Edimo <pedimonkoe@yahoo.com>
2022-06-07 16:54:47 +0200

    Creation des services de reviews

---
+++ review-service/src/main/java/com/example/reviewservice/services/ReviewService.java
@@ -0,0 +1,22 @@
+package com.example.reviewservice.services;
+
+import com.example.reviewservice.model.Review;
+import com.example.reviewservice.model.ReviewResponse;
+import com.example.reviewservice.model.ReviewResponseDto;
+
+import java.util.List;
+
+public interface ReviewService {
+
+    Review addReview(Review newReview);
+
+    Review editReview(Review newReview, long id);
+
+    void deleteReview(long id);
+
+    ReviewResponse getASingleReview(long id);
+
+    List<ReviewResponse> getAllReviews();
+
+    ReviewResponseDto getReviewByLocationId(int id);
+}
