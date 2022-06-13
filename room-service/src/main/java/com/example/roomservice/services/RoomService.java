0982c5ed2f8b04b3b833ddbba4783ac7c10b2c25
pierre Edimo <pedimonkoe@yahoo.com>
2022-06-13 11:21:25 +0200

    ajouter l'interface RoomService

---
+++ room-service/src/main/java/com/example/roomservice/services/RoomService.java
@@ -0,0 +1,22 @@
+package com.example.roomservice.services;
+
+import com.example.roomservice.model.Room;
+import com.example.roomservice.model.RoomOverViewDto;
+import com.example.roomservice.model.RoomResponse;
+
+import java.util.List;
+
+public interface RoomService {
+
+    RoomResponse getSingleRoom(long id);
+
+    Room editRoom(Room newRoom, long id);
+
+    Room createRoom(Room newRoom);
+
+    List<RoomResponse> getAllRooms();
+
+    RoomOverViewDto getRoomsByLocationId(long id);
+
+    void deleteRoom(long id);
+}
