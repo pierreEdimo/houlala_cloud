1023ea60c5dc4a69032381a925ffcafe15ab6e80
pierre Edimo <pedimonkoe@yahoo.com>
2022-06-13 11:27:17 +0200

    ajouter le Controllers pour Room

--- room-service/src/main/java/com/example/roomservice/RoomServiceApplication.java
+++ room-service/src/main/java/com/example/roomservice/RoomServiceApplication.java
@@ -1,13 +1,13 @@
-package com.example.hotelservice;
+package com.example.roomservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HotelServiceApplication {
+public class RoomServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HotelServiceApplication.class, args);
+        SpringApplication.run(RoomServiceApplication.class, args);
     }
 
 }
---
+++ room-service/src/main/java/com/example/roomservice/controllers/controllers.java
@@ -0,0 +1,42 @@
+package com.example.roomservice.controllers;
+
+import com.example.roomservice.model.Room;
+import com.example.roomservice.model.RoomResponse;
+import com.example.roomservice.services.RoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class controllers {
+
+    private final RoomService roomService;
+
+    @GetMapping("")
+    public List<RoomResponse> getAllRooms() {
+        return this.roomService.getAllRooms();
+    }
+
+    @PostMapping("/addRoom")
+    public Room addNewRoom(@RequestBody Room newRoom) {
+        return this.roomService.createRoom(newRoom);
+    }
+
+    @PutMapping("/editRoom/{id}")
+    public Room editRoom(@RequestBody Room newRoom, @PathVariable long id) {
+        return this.roomService.editRoom(newRoom, id);
+    }
+
+    @DeleteMapping("/deleteRoom/{id}")
+    public void deleteRoom(@PathVariable long id) {
+        this.roomService.deleteRoom(id);
+    }
+
+    @GetMapping("/singleRoom/{id}")
+    public RoomResponse getRoom(@PathVariable long id) {
+        return this.roomService.getSingleRoom(id);
+    }
+
+}
