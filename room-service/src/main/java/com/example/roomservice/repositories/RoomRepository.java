afe56c0bcf2f576a4fa9f7a8ae1f8029239de8dc
pierre Edimo <pedimonkoe@yahoo.com>
2022-06-13 11:31:04 +0200

    ajouter le RoomRepository

---
+++ room-service/src/main/java/com/example/roomservice/repositories/RoomRepository.java
@@ -0,0 +1,11 @@
+package com.example.roomservice.repositories;
+
+import com.example.roomservice.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    List<Room> findRoomsByLocationId(long id);
+}
