e208fc6b783676632b2ce1e0df94a051a71a9dc8
pierre Edimo <pedimonkoe@yahoo.com>
2022-06-15 13:43:12 +0200

    ajouter une Room dans le bootstrappData-

---
+++ room-service/src/main/java/com/example/roomservice/bootstrap/BootstrapData.java
@@ -0,0 +1,45 @@
+package com.example.roomservice.bootstrap;
+
+import com.example.roomservice.model.Room;
+import com.example.roomservice.model.RoomCategory;
+import com.example.roomservice.repositories.RoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class BootstrapData implements CommandLineRunner {
+
+    private final RoomRepository repository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Room room = new Room(
+                "edimo",
+                "lorem ipsuum indolor in samor ino dolor",
+                10,
+                7500,
+                new RoomCategory(
+                        "Suite de Luxe"
+                ),
+                2L
+        );
+
+        Room room2 = new Room(
+                "edimo",
+                "lorem ipsuum indolor in samor ino dolor",
+                10,
+                12000,
+                new RoomCategory(
+                        "Studio"
+                ),
+                2L
+        );
+
+        this.repository.save(room);
+        this.repository.save(room2);
+    }
+}
