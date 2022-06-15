e8cafb2e357a66da88028f41eb343caba82ae95a
pierre Edimo <pedimonkoe@yahoo.com>
2022-06-15 10:46:15 +0200

    ajouter l'objet RoomCategory dans le location-service

---
+++ location-service/src/main/java/com/example/locationService/model/RoomCategory.java
@@ -0,0 +1,11 @@
+package com.example.locationService.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RoomCategory {
+    private long id;
+    private String label;
+}
