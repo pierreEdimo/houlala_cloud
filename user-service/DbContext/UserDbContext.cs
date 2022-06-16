df74a91a41753634be02f22b2122f540bf10eaf2
pierreEdimo <pedimonkoe@yahoo.com>
2022-06-16 20:38:32 +0200

    Configurer les optiions pour l'utilisateur

---
+++ user-service/DbContext/UserDbContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore; 
+
+namespace user_service.DbContext
+{
+    public class UserDbContext : IdentityDbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+    }
+}
--- user-service/Program.cs
+++ user-service/Program.cs
@@ -1,4 +1,15 @@
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System.IdentityModel.Tokens.Jwt;
+using user_service.Model;
+using user_service.DbContext;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text; 
+
 var builder = WebApplication.CreateBuilder(args);
+IConfiguration Configuration = builder.Configuration;
 
 // Add services to the container.
 
@@ -6,6 +17,48 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddDbContext<UserDbContext>(options =>
+options.UseInMemoryDatabase("User"));
+
+builder.Services.AddHttpContextAccessor();
+
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+builder.Services.AddIdentity<User, Role>(config =>
+{
+    config.Password.RequireDigit = true;
+    config.Password.RequireLowercase = true;
+    config.Password.RequiredLength = 7;
+    config.Password.RequireNonAlphanumeric = true;
+    config.User.RequireUniqueEmail = true;
+})
+.AddEntityFrameworkStores<UserDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(config =>
+{
+    config.RequireHttpsMetadata = false;
+    config.SaveToken = true;
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        RequireExpirationTime = false,
+        ValidIssuer = Configuration["JwtIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtKey"])),
+        ClockSkew = TimeSpan.Zero
+    };
+});
 
 var app = builder.Build();
 
@@ -17,6 +70,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication(); 
 
 app.UseAuthorization();
 
