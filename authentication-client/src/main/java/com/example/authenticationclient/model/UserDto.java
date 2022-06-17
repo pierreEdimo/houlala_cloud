f6279f37b5c24cc6e1fbcc8b7791a924db049f01
pierre Edimo <pedimonkoe@yahoo.com>
2022-06-17 14:27:35 +0200

    ajouter UserDto

---
+++ authentication-client/src/main/java/com/example/authenticationclient/model/UserDto.java
@@ -0,0 +1,16 @@
+package com.example.authenticationclient.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDto {
+
+    private String email;
+
+    private String id;
+
+    private String userName;
+
+}
