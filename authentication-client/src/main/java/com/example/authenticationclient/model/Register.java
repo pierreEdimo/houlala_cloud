a6f3e708e2a53f4684d54897b897a3fcd26521de
pierre Edimo <pedimonkoe@yahoo.com>
2022-06-17 14:28:03 +0200

    ajouter Register.java

---
+++ authentication-client/src/main/java/com/example/authenticationclient/model/Register.java
@@ -0,0 +1,16 @@
+package com.example.authenticationclient.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Register {
+    
+    private String userName;
+
+    private String passWord;
+
+    private String email;
+
+}
