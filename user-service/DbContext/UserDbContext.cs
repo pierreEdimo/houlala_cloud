2a04c826963edeef398ee6d9e1ef827904a15331
pierreEdimo <pedimonkoe@yahoo.com>
2022-06-17 02:20:36 +0200

    ajouter lauthentication avec user-service

--- user-service/DbContext/UserDbContext.cs
+++ user-service/DbContext/UserDbContext.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore; 
+using user_service.Model; 
 
 namespace user_service.DbContext
 {
-    public class UserDbContext : IdentityDbContext
+    public class UserDbContext : IdentityDbContext<User>
     {
+        public UserDbContext() {}
         public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
     }
 }
---
+++ user-service/Dto/UserToken.cs
@@ -0,0 +1,9 @@
+namespace user_service.Dto
+{
+    public class UserToken
+    {
+        public UserDto? User { get; set; }
+        public String? Token { get; set; }
+    }
+
+}
