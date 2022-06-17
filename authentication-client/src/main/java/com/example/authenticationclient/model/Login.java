8bb6dbca78c7cbd884637fdb1d7f62e14008af8a
pierre Edimo <pedimonkoe@yahoo.com>
2022-06-17 14:28:19 +0200

    ajouter le Login.java

---
+++ authentication-client/src/main/java/com/example/authenticationclient/model/Login.java
@@ -0,0 +1,14 @@
+package com.example.authenticationclient.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Login {
+
+    private String email;
+
+    private String passWord;
+
+}
