b08d6130c2bf9d17d56a85231fa24cf8c163bfa5
pierreEdimo <pedimonkoe@yahoo.com>
2022-06-19 14:34:34 +0200

    ajouter le ProductType

---
+++ marketplace-client/src/main/java/com/example/marketplaceclient/model/ProductType.java
@@ -0,0 +1,11 @@
+package com.example.marketplaceclient.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductType {
+    private String _id;
+    private String label;
+}
