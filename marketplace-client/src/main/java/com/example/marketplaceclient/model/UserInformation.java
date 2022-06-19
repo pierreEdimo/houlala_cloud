96808c3b49f6752f70c38768482dcc71bbfb6306
pierreEdimo <pedimonkoe@yahoo.com>
2022-06-20 00:15:44 +0200

    ajouter le UserInformation,

---
+++ marketplace-client/src/main/java/com/example/marketplaceclient/model/UserInformation.java
@@ -0,0 +1,13 @@
+package com.example.marketplaceclient.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserInformation {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String telephoneNumber;
+}
