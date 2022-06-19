dadd719ba9ef14e262c2dcd842e14de5bbf98e70
pierreEdimo <pedimonkoe@yahoo.com>
2022-06-19 14:34:05 +0200

    ajouter le ProductTypeFeignClient

---
+++ marketplace-client/src/main/java/com/example/marketplaceclient/feign/ProductTypeFeignClient.java
@@ -0,0 +1,27 @@
+package com.example.marketplaceclient.feign;
+
+import com.example.marketplaceclient.exception.MarketplaceException;
+import com.example.marketplaceclient.model.ProductType;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@FeignClient(url = "https://houlala.herokuapp.com/api/product-types", name = "products")
+public interface ProductTypeFeignClient {
+
+    @GetMapping("")
+    List<ProductType> getAllProductTypes() throws MarketplaceException;
+
+    @GetMapping("/{id}")
+    ProductType getSingleProductType(@PathVariable String id) throws MarketplaceException;
+
+    @PostMapping("")
+    ProductType addProductType(@RequestBody ProductType newType) throws MarketplaceException;
+
+    @PatchMapping("/{id}")
+    ProductType editProductType(@RequestBody ProductType newType, @PathVariable String id) throws MarketplaceException;
+
+    @DeleteMapping("/{id}")
+    ProductType deleteProductType(@PathVariable String id) throws MarketplaceException;
+}
