1b7d3c7144ad46ae41484088d582c118fd0c701b
pierreEdimo <pedimonkoe@yahoo.com>
2022-06-19 14:35:40 +0200

    ajouter le ProductServiceFeignClient

--- marketplace-client/src/main/java/com/example/marketplaceclient/feign/ProductServiceFeignClient.java
+++ marketplace-client/src/main/java/com/example/marketplaceclient/feign/ProductServiceFeignClient.java
@@ -2,8 +2,9 @@ package com.example.marketplaceclient.feign;
 
 import com.example.marketplaceclient.exception.MarketplaceException;
 import com.example.marketplaceclient.model.Product;
+import com.example.marketplaceclient.model.ProductResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -11,5 +12,30 @@ import java.util.List;
 public interface ProductServiceFeignClient {
 
     @GetMapping("")
-    List<Product> getAllProducts() throws MarketplaceException;
+    List<ProductResponse> getAllProducts() throws MarketplaceException;
+
+    @GetMapping("/{id}")
+    ProductResponse getSingleProduct(@PathVariable String id) throws MarketplaceException;
+
+    @GetMapping("/search")
+    List<ProductResponse> searchProduct(@RequestParam String searchWord) throws MarketplaceException;
+
+    @GetMapping("/getRandomProducts")
+    List<ProductResponse> getRandomProducts(@RequestParam int size, @RequestParam String categoryId) throws MarketplaceException;
+
+    @GetMapping("/filterProductByPageId")
+    List<ProductResponse> getProductByPageId(@RequestParam String pageId, @RequestParam int limit) throws MarketplaceException;
+
+    @GetMapping("/filterProductsByCategoryId")
+    List<ProductResponse> getProductByCategoryId(@RequestParam String categoryId, @RequestParam int limit) throws MarketplaceException;
+
+    @GetMapping("/filterProductsByCategoryAndProductType")
+    List<ProductResponse> getProductsByTypeAndCategoryId(@RequestParam String categoryId, @RequestParam String productType) throws MarketplaceException;
+
+    @DeleteMapping("/{id}")
+    ProductResponse deleteProduct(@PathVariable String id) throws MarketplaceException;
+
+    @PatchMapping("/{id}")
+    ProductResponse editProduct(@PathVariable String id, @RequestBody Product editProduct) throws MarketplaceException;
+
 }
