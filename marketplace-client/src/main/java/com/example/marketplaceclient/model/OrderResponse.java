342651421f3de10874531fb9e57d0ea82fc0532a
pierre Edimo <pedimonkoe@yahoo.com>
2022-06-20 01:19:05 +0200

    ajouter le OrderResponse.java

---
+++ marketplace-client/src/main/java/com/example/marketplaceclient/model/OrderResponse.java
@@ -0,0 +1,31 @@
+package com.example.marketplaceclient.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+public class OrderResponse {
+    private List<CartItemResponse> cartItems;
+
+    private int totalQuantity;
+
+    private int totalPrice;
+
+    private String status;
+
+    private String payMentMode;
+
+    private String orderOptions;
+
+    private UserInformation userInformation;
+
+    private Address address;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
