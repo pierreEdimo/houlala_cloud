9f2369f4f4459caf90eb8cf63709f9a9fda8d3a8
pierreEdimo <pedimonkoe@yahoo.com>
2022-06-19 17:38:17 +0200

    creer le model Category

---
+++ marketplace-client/src/main/java/com/example/marketplaceclient/model/Category.java
@@ -0,0 +1,16 @@
+package com.example.marketplaceclient.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Category {
+    private String _id;
+
+    private String name;
+
+    private String imageUrl;
+
+    private String description;
+}
