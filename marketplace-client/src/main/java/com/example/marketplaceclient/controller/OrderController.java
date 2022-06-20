9920c311898ad048db56ca5ee8b4c1f30bbf1d2b
pierre Edimo <pedimonkoe@yahoo.com>
2022-06-20 09:38:40 +0200

    ajouter les methodes sendOrder et getOrdersFromUsers

--- marketplace-client/src/main/java/com/example/marketplaceclient/controller/OrderController.java
+++ marketplace-client/src/main/java/com/example/marketplaceclient/controller/OrderController.java
@@ -5,9 +5,7 @@ import com.example.marketplaceclient.feign.OrderServiceFeignClient;
 import com.example.marketplaceclient.model.Order;
 import com.example.marketplaceclient.model.OrderResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -23,6 +21,24 @@ public class OrderController {
     public List<OrderResponse> getAllOrders() {
         try {
             return this.feignClient.getAllOrders();
+        } catch (MarketplaceException e) {
+            throw new ResponseStatusException(e.getHttpStatus(), e.getMessage());
+        }
+    }
+
+    @PostMapping("")
+    public OrderResponse sendOrder(@RequestBody Order newOrder){
+        try {
+            return this.feignClient.sendOrders(newOrder);
+        } catch (MarketplaceException e) {
+            throw new ResponseStatusException(e.getHttpStatus(), e.getMessage());
+        }
+    }
+
+    @GetMapping("/getOrdersFromUsers")
+    public List<OrderResponse> getOrdersFromUsers(@RequestParam String email){
+        try {
+            return this.feignClient.getOrdersFromUsers(email);
         } catch (MarketplaceException e) {
             throw new ResponseStatusException(e.getHttpStatus(), e.getMessage());
         }
