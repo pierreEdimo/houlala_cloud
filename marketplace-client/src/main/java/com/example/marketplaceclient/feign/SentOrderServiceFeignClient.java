8f783c839ee178612a4cc40674acee3c701f72a9
pierre Edimo <pedimonkoe@yahoo.com>
2022-06-20 09:39:42 +0200

    ajouter le SentOrderServiceFeignClient

---
+++ marketplace-client/src/main/java/com/example/marketplaceclient/feign/SentOrderServiceFeignClient.java
@@ -0,0 +1,29 @@
+package com.example.marketplaceclient.feign;
+
+import com.example.marketplaceclient.exception.MarketplaceException;
+import com.example.marketplaceclient.model.SentOrder;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(url = "https://houlala.herokuapp.com/api/sent-orders", name = "sent-orders")
+public interface SentOrderServiceFeignClient {
+
+    @GetMapping("")
+    List<SentOrder> getAllOrders() throws MarketplaceException;
+
+    @GetMapping("/getSentOrdersByEmail")
+    List<SentOrder> getOrdersByEmail(@RequestParam String email ) throws MarketplaceException;
+
+    @GetMapping("/getAllSentOrdersFromPageId")
+    List<SentOrder> getOrdersByLocationId(@RequestParam String locationId) throws MarketplaceException;
+
+    @GetMapping("/getSentOrdersByPageId")
+    List<SentOrder> getOrdersByLocationIdAndToday(@RequestParam String locationId) throws MarketplaceException;
+
+    @GetMapping("/filterOrderByStatus")
+    List<SentOrder> getOrderByStatus(@RequestParam String locationId, @RequestParam String status) throws MarketplaceException;
+
+}
