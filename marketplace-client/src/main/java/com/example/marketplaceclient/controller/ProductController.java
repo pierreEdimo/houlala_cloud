9aa81b4da717619a0e6124908f25f32ca8f4f3a9
pierre Edimo <pedimonkoe@yahoo.com>
2022-06-20 11:48:12 +0200

    ajouter de nouvelles Methodes dans le nouveau Controller

--- marketplace-client/src/main/java/com/example/marketplaceclient/controller/ProductController.java
+++ marketplace-client/src/main/java/com/example/marketplaceclient/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.example.marketplaceclient.feign.ProductServiceFeignClient;
 import com.example.marketplaceclient.model.Product;
 import com.example.marketplaceclient.model.ProductResponse;
 import com.example.marketplaceclient.services.ProductService;
+import feign.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -16,7 +17,7 @@ import java.util.List;
 @RequestMapping("/products")
 public class ProductController {
 
-    private final ProductService productService; 
+    private final ProductService productService;
 
     @GetMapping("")
     public List<ProductResponse> getAllProducts() {
@@ -24,39 +25,58 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public ProductResponse getSingleProduct(@PathVariable String id){
+    public ProductResponse getSingleProduct(@PathVariable String id) {
         return this.productService.getProduct(id);
     }
 
     @GetMapping("/search")
-    public List<ProductResponse> searchProducts(@RequestParam String searchWord){
+    public List<ProductResponse> searchProducts(@RequestParam String searchWord) {
         return this.productService.searchProduct(searchWord);
     }
 
     @GetMapping("/getRandomProducts")
-    public List<ProductResponse> getRandomProducts(@RequestParam int size, @RequestParam String categoryId){
+    public List<ProductResponse> getRandomProducts(@RequestParam int size, @RequestParam String categoryId) {
         return this.productService.getRandomProducts(size, categoryId);
     }
 
     @GetMapping("/filterProductsByCategoryId")
-    public List<ProductResponse> getProductsByCategoryId(@RequestParam String categoryId, @RequestParam int limit){
+    public List<ProductResponse> getProductsByCategoryId(@RequestParam String categoryId, @RequestParam int limit) {
         return this.productService.getProductsByCategoryId(categoryId, limit);
     }
 
     @GetMapping("/filterProductsByCategoryAndProductType")
-    public List<ProductResponse> getProductsByTypeAndCategoryId(@RequestParam String categoryId, @RequestParam String productType){
+    public List<ProductResponse> getProductsByTypeAndCategoryId(@RequestParam String categoryId, @RequestParam String productType) {
         return this.productService.getProductsByTypeAndCategoryId(categoryId, productType);
     }
 
     @DeleteMapping("/{id}")
-    public ProductResponse deleteProduct(@PathVariable String id){
+    public ProductResponse deleteProduct(@PathVariable String id) {
         return this.productService.deleteProduct(id);
     }
 
     @PatchMapping("/{id}")
-    public ProductResponse editProduct(@PathVariable String id, @RequestBody Product newProduct){
+    public ProductResponse editProduct(@PathVariable String id, @RequestBody Product newProduct) {
         return this.productService.editProduct(id, newProduct);
     }
 
+    @PatchMapping("/addFavorite/{id}")
+    public void addProductToFavorite(@PathVariable String id, @RequestParam String userId) {
+        this.productService.addProductToFavorite(userId, id);
+    }
+
+    @GetMapping("/favorites")
+    public List<ProductResponse> getFavoritesProduct(@RequestParam String userId) {
+        return this.productService.getFavoritesProduct(userId);
+    }
+
+    @GetMapping("/filterProductByPageId")
+    public List<ProductResponse> getProductByLocationId(@RequestParam String locationId, @RequestParam int limit) {
+        return this.productService.getProductByLocationId(locationId, limit);
+    }
+
+    @GetMapping("/productId/{id}")
+    public ProductResponse getProductByIdAndIsFavorite(@PathVariable String id, @RequestParam String userId) {
+        return this.productService.getProductByIdAndIsFavorite(id, userId);
+    }
 
 }
