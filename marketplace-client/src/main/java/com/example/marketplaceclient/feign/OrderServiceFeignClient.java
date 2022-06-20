c501dc228fed133f70f78cf92a97332fb2d65838
pierre Edimo <pedimonkoe@yahoo.com>
2022-06-20 09:38:49 +0200

    ajouter les methodes sendOrder et getOrdersFromUsers

--- marketplace-client/src/main/java/com/example/marketplaceclient/feign/OrderServiceFeignClient.java
+++ marketplace-client/src/main/java/com/example/marketplaceclient/feign/OrderServiceFeignClient.java
@@ -5,6 +5,9 @@ import com.example.marketplaceclient.model.Order;
 import com.example.marketplaceclient.model.OrderResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -13,4 +16,10 @@ public interface OrderServiceFeignClient {
 
     @GetMapping("")
     List<OrderResponse> getAllOrders() throws MarketplaceException;
+
+    @PostMapping("")
+    OrderResponse sendOrders(@RequestBody Order newOrder) throws MarketplaceException;
+
+    @GetMapping("/getOrdersFromUsers")
+    List<OrderResponse> getOrdersFromUsers(@RequestParam String email) throws MarketplaceException;
 }
