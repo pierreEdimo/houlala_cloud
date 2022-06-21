b367a2d94c95111df81ea8f565c12d6bce8bb00f
pierre Edimo <pedimonkoe@yahoo.com>
2022-06-21 13:13:41 +0200

    ajouter la methode getAllProductInfos dans le InventoryServiceFeignClient

--- marketplace-client/src/main/java/com/example/marketplaceclient/feign/InventoryServiceFeignClient.java
+++ marketplace-client/src/main/java/com/example/marketplaceclient/feign/InventoryServiceFeignClient.java
@@ -6,10 +6,15 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @FeignClient(name = "inventory", path = "inventory")
 public interface InventoryServiceFeignClient {
 
-    @GetMapping("/{id}")
+    @GetMapping("/products/{id}")
     ProductAdditionalInformation getASingleInfo(@PathVariable String id) throws MarketplaceException;
+
+    @GetMapping("/products")
+    List<ProductAdditionalInformation> getAllProductInfos() throws MarketplaceException;
 
 }
