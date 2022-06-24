7198218eec0972a06dc05a8d8c1f0a75ec4706ab
pierre Edimo <pedimonkoe@yahoo.com>
2022-06-24 13:48:17 +0200

    changer le RequestParam en RequestPart

--- upload-service/src/main/java/com/example/uploadservice/controller/Controller.java
+++ upload-service/src/main/java/com/example/uploadservice/controller/Controller.java
@@ -2,10 +2,10 @@ package com.example.uploadservice.controller;
 
 import com.example.uploadservice.service.UploadService;
 import lombok.RequiredArgsConstructor;
-import org.apache.http.Header;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,8 +19,8 @@ public class Controller {
     private final UploadService service;
 
 
-    @PostMapping("/upload")
-    public String uploadFile(@RequestParam(name = "file")MultipartFile file){
+    @PostMapping("/newFile")
+    public String uploadFile(@RequestPart(value = "file") MultipartFile file){
         Resource resource;
         String url = "";
         try {
