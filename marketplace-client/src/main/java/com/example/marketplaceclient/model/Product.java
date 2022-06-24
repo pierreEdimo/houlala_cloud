0be897edc39279ee310ab9a45916a7d033d1a7ca
pierre Edimo <pedimonkoe@yahoo.com>
2022-06-24 13:46:50 +0200

    creation de product.java

--- marketplace-client/src/main/java/com/example/marketplaceclient/model/Product.java
+++ marketplace-client/src/main/java/com/example/marketplaceclient/model/Product.java
@@ -3,14 +3,38 @@ package com.example.marketplaceclient.model;
 import lombok.Getter;
 import lombok.Setter;
 
+
 @Getter
 @Setter
 public class Product {
+    private String _id;
     private String name;
     private String description;
     private int weight;
+    private String imageUrl;
     private int sellingPrice;
     private String locationId;
-    private String productType;
-    private String categoryId;
+    private boolean bookMarked;
+    private String productSku;
+
+
+
+    public Product(){}
+
+    public Product(
+            String name,
+            String description,
+            int weight,
+            int sellingPrice,
+            String locationId,
+            String productSku
+    ){
+        this.name = name;
+        this.description = description;
+        this.weight = weight;
+        this.sellingPrice = sellingPrice;
+        this.locationId = locationId;
+        this.productSku = productSku;
+    }
 }
+
