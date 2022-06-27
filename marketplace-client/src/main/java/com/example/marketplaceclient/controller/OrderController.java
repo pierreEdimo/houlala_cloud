009e8dfc9c2af1edde4b0dfeee8879f3a5348160
pierre Edimo <pedimonkoe@yahoo.com>
2022-06-27 08:23:35 +0200

    changer OrderResponse en OrderDto

--- marketplace-client/src/main/java/com/example/marketplaceclient/controller/OrderController.java
+++ marketplace-client/src/main/java/com/example/marketplaceclient/controller/OrderController.java
@@ -3,7 +3,7 @@ package com.example.marketplaceclient.controller;
 import com.example.marketplaceclient.exception.MarketplaceException;
 import com.example.marketplaceclient.feign.OrderServiceFeignClient;
 import com.example.marketplaceclient.model.Order;
-import com.example.marketplaceclient.model.OrderResponse;
+import com.example.marketplaceclient.model.OrderDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -18,7 +18,7 @@ public class OrderController {
     private final OrderServiceFeignClient feignClient;
 
     @GetMapping("")
-    public List<OrderResponse> getAllOrders() {
+    public List<OrderDto> getAllOrders() {
         try {
             return this.feignClient.getAllOrders();
         } catch (MarketplaceException e) {
@@ -27,7 +27,7 @@ public class OrderController {
     }
 
     @PostMapping("")
-    public OrderResponse sendOrder(@RequestBody Order newOrder){
+    public OrderDto sendOrder(@RequestBody Order newOrder){
         try {
             return this.feignClient.sendOrders(newOrder);
         } catch (MarketplaceException e) {
@@ -36,7 +36,7 @@ public class OrderController {
     }
 
     @GetMapping("/getOrdersFromUsers")
-    public List<OrderResponse> getOrdersFromUsers(@RequestParam String email){
+    public List<OrderDto> getOrdersFromUsers(@RequestParam String email){
         try {
             return this.feignClient.getOrdersFromUsers(email);
         } catch (MarketplaceException e) {
