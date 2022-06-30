85ece279d0abefb1939aee8f14832295f37e6443
pierre Edimo <pedimonkoe@yahoo.com>
2022-06-30 09:51:30 +0200

    Ajouter le error-decoder

--- marketplace-client/src/main/java/com/example/marketplaceclient/errorDecoder/MarketplaceErrorDecoder.java
+++ marketplace-client/src/main/java/com/example/marketplaceclient/errorDecoder/MarketplaceErrorDecoder.java
@@ -1,16 +1,15 @@
 package com.example.marketplaceclient.errorDecoder;
 
-
 import com.example.marketplaceclient.exception.MarketplaceException;
 import feign.Response;
 import feign.codec.ErrorDecoder;
 import org.springframework.http.HttpStatus;
 
+
 public class MarketplaceErrorDecoder implements ErrorDecoder {
 
     @Override
-    public Exception decode(String s, Response response) {
-        return new MarketplaceException("Error by Calling the MarketPlace-Service",
-                HttpStatus.resolve(response.status()));
+    public Exception decode(String methodKey, Response response) {
+        return new MarketplaceException("error", HttpStatus.resolve(response.status()));
     }
 }
