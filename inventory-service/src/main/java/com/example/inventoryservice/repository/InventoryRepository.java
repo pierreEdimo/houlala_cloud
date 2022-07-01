5b97826d7f097d2efc15bd8f73b9c0c76a9d87bf
pierre Edimo <pedimonkoe@yahoo.com>
2022-07-01 15:15:50 +0200

    Ajouter le InventoryRepository.java

---
+++ inventory-service/src/main/java/com/example/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,12 @@
+package com.example.inventoryservice.repository;
+
+import com.example.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    List<Inventory> findInventoriesByLocationId(String locationId);
+
+}
