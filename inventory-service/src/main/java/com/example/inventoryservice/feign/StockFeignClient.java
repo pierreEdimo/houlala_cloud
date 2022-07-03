94cfb48bc51b0e9225bb63858ec3b03378716062
pierre Edimo <pedimonkoe@yahoo.com>
2022-07-03 22:53:34 +0200

    la methode getProductByInfo dans le StockFeignClient

---
+++ inventory-service/src/main/java/com/example/inventoryservice/feign/StockFeignClient.java
@@ -0,0 +1,17 @@
+package com.example.inventoryservice.feign;
+
+
+import com.example.inventoryservice.exception.InventoryException;
+import com.example.inventoryservice.model.ProductInformation;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "stock", path = "stock")
+public interface StockFeignClient {
+
+    @GetMapping("/getInfoBySku")
+    ProductInformation getProductBySKu(@RequestParam String productSku) throws InventoryException;
+
+
+}
