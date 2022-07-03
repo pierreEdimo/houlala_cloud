6b461935a6d0fd6e0464382181291d698afc0b54
pierre Edimo <pedimonkoe@yahoo.com>
2022-07-03 22:50:26 +0200

    Refactoring du Product.java

--- inventory-service/src/main/java/com/example/inventoryservice/model/Product.java
+++ inventory-service/src/main/java/com/example/inventoryservice/model/Product.java
@@ -3,24 +3,14 @@ package com.example.inventoryservice.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
 
-@Entity
 @Getter
 @Setter
 public class Product {
 
-    @Id
     private String productSku;
 
+    private int quantity;
+
     private String name;
-
-    private String imageUrl;
-
-    private int solQuantity;
-
-    private int isQuantity;
-
-    private int deficit;
 }
