3fbb01285793c2960d8bf6ce785002e1f770d1ec
pierre Edimo <pedimonkoe@yahoo.com>
2022-07-17 00:59:42 +0200

    Ajouter l'Objet Location.java dans le PostService

---
+++ marketplace-client/src/main/java/com/example/marketplaceclient/model/Location.java
@@ -0,0 +1,12 @@
+package com.example.marketplaceclient.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Location {
+    private String name;
+    private long id;
+    private String imageUrl = "https://images.unsplash.com/photo-1657891753725-6bdc1fe511c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80";
+}
