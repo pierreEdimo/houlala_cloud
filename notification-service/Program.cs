504cdbcb504105738588002f1301450c13f71d0c
pierre Edimo <pedimonkoe@yahoo.com>
2022-07-17 22:36:37 +0200

    configuration

--- notification-service/Program.cs
+++ notification-service/Program.cs
@@ -9,8 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IEmailService, EmailService>(); 
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
