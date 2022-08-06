3a6a5a6796b11700146d5f44395f0d6f97bf79a0
pierre Edimo <pedimonkoe@yahoo.com>
2022-08-06 17:25:50 +0200

    Changer userId en Id.

--- marketplace-client/src/main/java/com/example/marketplaceclient/model/UserInformation.java
+++ marketplace-client/src/main/java/com/example/marketplaceclient/model/UserInformation.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserInformation {
-    private String userId;
+    private String id;
     private String name;
     private String email;
     private String phoneNumber;
