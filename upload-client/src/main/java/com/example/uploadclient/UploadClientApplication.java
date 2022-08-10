c63fed5280d12cc88a97e7f8b97a1ab36d3e2d48
pierre Edimo <pedimonkoe@yahoo.com>
2022-08-10 16:54:55 +0200

    Ajouter l'Upload-client

---
+++ upload-client/src/main/java/com/example/uploadclient/UploadClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.uploadclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadClientApplication.class, args);
+    }
+
+}
