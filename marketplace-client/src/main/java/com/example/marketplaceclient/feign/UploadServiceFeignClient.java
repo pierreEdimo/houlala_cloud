2e353f4ff24c02f342953e38a55aaa1914d0274e
pierre Edimo <pedimonkoe@yahoo.com>
2022-11-08 00:38:55 +0100

    changer l'url du upload service

--- marketplace-client/src/main/java/com/example/marketplaceclient/feign/UploadServiceFeignClient.java
+++ marketplace-client/src/main/java/com/example/marketplaceclient/feign/UploadServiceFeignClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 
-@FeignClient(url = "http://houlala_storage:8001", name = "upload", configuration = FeignSupportConfig.class)
+@FeignClient(url = "https://upload.houlala.store", name = "upload", configuration = FeignSupportConfig.class)
 public interface UploadServiceFeignClient {
 
     @PostMapping(value = "/api/Upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
