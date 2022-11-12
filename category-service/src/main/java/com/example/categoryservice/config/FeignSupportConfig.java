d75896e6fb742dab76770d825d697a5fdd80dd7a
pierre Edimo <pedimonkoe@yahoo.com>
2022-11-13 00:24:21 +0100

    configurer le FeignSupportConfig

--- category-service/src/main/java/com/example/categoryservice/config/FeignSupportConfig.java
+++ category-service/src/main/java/com/example/categoryservice/config/FeignSupportConfig.java
@@ -1,6 +1,8 @@
 package com.example.categoryservice.config;
 
 import feign.form.spring.SpringFormEncoder;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +11,12 @@ import org.springframework.web.client.RestTemplate;
 public class FeignSupportConfig {
     @Bean
     public SpringFormEncoder multipartFormEncoder() {
-        return new SpringFormEncoder(new SpringEncoder(() -> new HttpMessageConverters(new RestTemplate().getMessageConverters())));
+        return new SpringFormEncoder(new SpringEncoder(new ObjectFactory<HttpMessageConverters>() {
+            @Override
+            public HttpMessageConverters getObject() throws BeansException {
+                return new HttpMessageConverters(new RestTemplate().getMessageConverters());
+            }
+        }));
     }
 }
+
