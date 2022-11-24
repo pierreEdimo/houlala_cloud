26a0f53516140d46292cad2e7d81060a4d64b1fc
pierre Edimo <pedimonkoe@yahoo.com>
2022-11-24 09:14:03 +0100

    Configurer les autres envoyeurs d'Email

--- notification-client/src/main/java/com/example/notificationclient/service/EmailService.java
+++
@@ -1,7 +0,0 @@
-package com.example.notificationclient.service;
-
-import com.example.notificationclient.model.dto.SendEmaildto;
-
-public interface EmailService {
-    void sendEmail(SendEmaildto newEmailDto);
-}
